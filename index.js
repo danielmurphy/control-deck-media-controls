18c63cc6ae6f2b03305e12415c8dfe65d6de1964
Daniel Murphy <danielmurphy02@gmail.com>
2018-12-13 15:01:48 -0800

    init

---
+++ index.js
@@ -0,0 +1,44 @@
+const robot = require('robotjs');
+const path = require('path');
+
+class ControlDeckMediaControls {
+	constructor(streamDeck, buttonId, options) {
+		console.log(options);
+
+		let key = '';
+		let icon = '';
+
+		switch (options.type) {
+		case 'play':
+			key = 'audio_play';
+			icon = 'play.png';
+			break;
+		case 'volume_up':
+			key = 'audio_vol_up';
+			icon = 'volume_up.png';
+			break;
+		case 'volume_down':
+			key = 'audio_vol_down';
+			icon = 'volume_down.png';
+			break;
+		case 'mute':
+			key = 'audio_mute';
+			icon = 'mute.png';
+			break;
+		}
+
+		streamDeck.fillImageFromFile(
+			buttonId,
+			path.resolve(__dirname, 'icons/' + icon)
+		);
+
+		streamDeck.on('up', keyIndex => {
+			if (keyIndex === buttonId) {
+				console.log(`pressing ${key}`);
+				robot.keyTap(key);
+			}
+		});
+	}
+}
+
+module.exports = ControlDeckMediaControls;
